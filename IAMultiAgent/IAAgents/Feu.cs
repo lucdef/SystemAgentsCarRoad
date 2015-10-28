84e72c846141ad97f05844f300d6f8486e486ffa
Eredrim <gilles.moriani@epsi.fr>
2015-10-28 17:33:45 +0100

    Classe Feu

---
+++ IAMultiAgent/IAAgents/Feu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAAgents
+{
+    class Feu
+    {
+        bool isVert { get; set; }
+        TimeSpan tempsRouge { get; set; }
+        TimeSpan tempsVert { get; set; }
+
+        public Feu(bool isVert, TimeSpan tRouge, TimeSpan tVert)
+        {
+            this.isVert = isVert;
+            this.tempsRouge = tRouge;
+            this.tempsVert = tVert;
+        }
+    }
+}
