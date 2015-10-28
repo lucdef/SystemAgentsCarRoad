3089f6a8af6b7b51ff03489a5e05b2a533e4fb39
Eredrim <gilles.moriani@epsi.fr>
2015-10-28 17:16:45 +0100

    First

---
+++ IAMultiAgent/IAAgents/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAAgents
+{
+    class Position
+    {
+        double x {get; set;}
+        double y {get; set;}
+
+        public Position(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ IAMultiAgent/IAAgents/Vehicule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAAgents
+{
+    enum Direction
+    {
+        G,
+        D,
+        F
+    }
+
+    public class Vehicule
+    {
+        float longueur;
+        float largeur;
+        float vMax;
+        Direction dir;
+
+        Position pos;
+        Vehicule vDevant; 
+        int a;
+        bool arret;
+
+        public Vehicule(float longueur, float largeur, float vMax, Direction dir, Position pos)
+        {
+            this.longueur = longueur;
+            this.largeur = largeur;
+            this.vMax = vMax;
+            this.dir = dir;
+            this.pos = pos;
+            this.vDevant = null;
+            this.arret = false;
+        }
+    }
+}
